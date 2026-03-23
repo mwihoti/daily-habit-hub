8b9d1d112d64f7e8b0de5c32ba864f61e17bc307
mwihoti <danielmwihoti@gmail.com>
2026-03-23 09:10:48 +0300

    Fix web3 production build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,24 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@rainbow-me/rainbowkit', 'wagmi', 'viem'],
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        perf_hooks: false,
+        os: false,
+        child_process: false,
+        worker_threads: false,
+        cluster: false,
+        readline: false,
+      };
+    }
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
 
 export default nextConfig;
